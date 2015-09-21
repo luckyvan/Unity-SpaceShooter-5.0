14c230bbd401d9ace1ff6156c2eab92828dfc754
Fan Chou <Chou.Fan@igt.com>
2015-09-21 17:06:24 +0800

    3.2.1 Add PlayerController.cs

---
+++ SpaceShooter/Assets/_Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody rigidBody;
+
+	void Start(){
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rigidBody.velocity = movement;
+	}
+}
