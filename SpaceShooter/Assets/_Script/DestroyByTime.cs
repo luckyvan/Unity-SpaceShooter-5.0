1cbc666994947b2a3c26690a5d7792021ac82dc6
Fan Chou <Chou.Fan@igt.com>
2015-09-22 14:38:41 +0800

    3.2.3 random generated asteroid and control explosion life cycle.

---
+++ SpaceShooter/Assets/_Script/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+	
+	public float lifeTime = 2.0f;
+	
+	void Start(){
+
+		Destroy (gameObject, lifeTime);
+
+	}
+}
---
+++ SpaceShooter/Assets/_Script/GameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+#region public field
+	public GameObject hazard; // prefab used to generate asteroid
+
+	public Vector3 spawnValue = new Vector3(6.5f, 0f, 15f);
+
+	public int hazardCount = 10;
+
+	public float spawnWait = 0.5f;
+
+	public float startWait = 2f;
+
+	public float waveWait = 2f;
+#endregion
+
+#region private field
+	private Quaternion spawnRotation;
+	
+	public Vector3 spawnPosition = Vector3.zero;
+#endregion
+
+	IEnumerator spawnWaves(){
+		yield return new WaitForSeconds(startWait);
+
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				spawnPosition.x = Random.Range (-spawnValue.x, spawnValue.x);
+				spawnPosition.z = spawnValue.z;
+				spawnRotation = Quaternion.identity;
+				
+				Instantiate (hazard, spawnPosition, spawnRotation);	
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawnWaves ());
+	}
+
+}
