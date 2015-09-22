215674e5c282fde16c8b1362515c9e49dc85d025
Fan Chou <Chou.Fan@igt.com>
2015-09-22 08:57:30 +0800

    3.2.3 add asteroid with random angular velocity

---
+++ SpaceShooter/Assets/_Script/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	Rigidbody rigidBody;
+
+	public float tumble = 10.0f;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		
+		if (rigidBody != null) {
+			rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+		}else{
+			throw new UnityException("No RigidBody Added to Player");
+		}
+	}
+
+}
