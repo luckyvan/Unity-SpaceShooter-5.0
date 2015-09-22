2ca7cfa925e3a4b2e2c09b412a24ec929db0cc4c
Fan Chou <Chou.Fan@igt.com>
2015-09-22 10:49:04 +0800

    3.2.3 add explosion

--- SpaceShooter/Assets/_Script/DestroyByContacts.cs
+++ SpaceShooter/Assets/_Script/DestroyByContacts.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 
 public class DestroyByContacts : MonoBehaviour {
-	
+
+	public GameObject explosion;
+
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter (Collider other){
 		if (other.gameObject.tag != "Boundary") {
 			Destroy (other.gameObject);
 			Destroy (gameObject);	
+
+			Instantiate (explosion, gameObject.transform.position, gameObject.transform.rotation);
+
+			if (other.tag == "Player") {
+				Instantiate (playerExplosion, other.gameObject.transform.position, other.gameObject.transform.rotation);
+			}
 		}
 	}
 }
